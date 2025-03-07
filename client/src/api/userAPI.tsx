04fbed6a6d9fdba19b9ed390433096fee33bfbc1
heliamosa <heliamosayebian@yahoo.com>
2025-03-06 18:35:41 -0600

    Initial commit - Secure Kanban with JWT auth

---
+++ client/src/api/userAPI.tsx
@@ -0,0 +1,12 @@
+const retrieveUsers = async () => {
+  try {
+    // TODO: Add a fetch to request the user data from the server
+    // * Hint the api endpoint is at '/api/users'
+
+  } catch (err) {
+    console.log('Error from data retrieval:', err);
+    return [];
+  }
+};
+
+export { retrieveUsers };
